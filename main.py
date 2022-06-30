b52c0b9d861612db511c5082955336629bd50fb6
dmunofer <diego.fmz@hotmail.com>
2022-06-30 13:15:41 +0200

    squad is undefeated main

--- main.py
+++ main.py
@@ -9,8 +9,8 @@ from Superheroes import Superheroes
 
 
 
- def get_Superheroe_in_a_list_of_Superheroes(player,list_of_heroes):
-    
+def get_Superheroe_in_a_list_of_Superheroes(player,list_of_heroes):
+
     final=[]
     for heroe in list_of_heroes:
         if heroe.is_alive():
@@ -18,3 +18,11 @@ from Superheroes import Superheroes
     for heroe in final:
         print(heroe)
     return final
+
+def squad_is_undefeated(list_of_heroes):
+    is_undefeated= False
+    for heroe in list_of_heroes:
+        if heroe.is_alive():
+            is_undefeated=True
+    return is_undefeated
+
