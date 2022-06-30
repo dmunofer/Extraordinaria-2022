9a5e77a170527ee968760c8779413f1b9a16b333
dmunofer <diego.fmz@hotmail.com>
2022-06-30 13:09:45 +0200

    movimientos fight attack y fight defense

--- Movimientos.py
+++ Movimientos.py
@@ -3,12 +3,18 @@ from Superheroes import Superheroes
 
 class Movimientos():
 
-   def fight_defense(self,points_of_damage):
+    mov_attack= int(Superheroes.fuerza)*0.8+int(Superheroes.velocidad)+int(Superheroes.hab_lucha)*0.75+int(Superheroes.proy_energia)
+    mov_def= int(Superheroes.inteligencia)+int(Superheroes.velocidad)*0.75+int(Superheroes.hab_lucha)*0.25+int(Superheroes.fuerza)*0.2
+
+
+
+
+def fight_defense(self,points_of_damage):
        if points_of_damage<=Superheroes.resistencia:
            return False
        else:
            Superheroes.energia_vital -=(points_of_damage-Superheroes.resistencia)
 
 
-   def fight_attack(self, superheroe_to_attack):
+def fight_attack(self, superheroe_to_attack):
        return superheroe_to_attack.fight_defense(Superheroes.fuerza)
