bfecca5da43403e9116928e0ff5d9546c80b6cef
dmunofer <diego.fmz@hotmail.com>
2022-06-30 12:57:47 +0200

    clase movimientos fight attack y defense

--- Movimientos.py
+++ Movimientos.py
@@ -1,5 +1,14 @@
-
+from Superheroes import Superheroes
 
 
 class Movimientos():
-    
+
+   def fight_defense(self,points_of_damage):
+       if points_of_damage<=Superheroes.resistencia:
+           return False
+       else:
+           Superheroes.energia_vital -=(points_of_damage-Superheroes.resistencia)
+
+
+   def fight_attack(self, superheroe_to_attack):
+       return superheroe_to_attack.fight_defense(Superheroes.fuerza)
