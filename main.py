318a3ec27bb10d657597d0935b983342696030b5
dmunofer <diego.fmz@hotmail.com>
2022-06-30 13:27:03 +0200

    get data from user main

--- main.py
+++ main.py
@@ -2,6 +2,23 @@ from Superheroes import Superheroes
 
 
 
+def get_data_from_user(filename):
+    file= open(filename, "r")
+    final= []
+    for line in file.readlines():
+      data= line.split(",")
+      id= int(data[0])
+      alias= data[1]
+      id_secret= int(data[3])
+      inteligencia= int(data[4])
+      fuerza= int(data[5])
+      velocidad= int(data[6])
+      resistencia= int(data[7])
+      proy_energia= int(data[8])
+      hab_lucha= int(data[9])
+      superheroe= Superheroes(id,id, alias, id_secret, inteligencia, fuerza, velocidad, resistencia, proy_energia, hab_lucha)
+      final.append(superheroe)
+    return final
 
 
 
